5c918ece74d15ce7c476ce566a115ea522126cfc
asm128 <pabloarielz@outlook.com>
2019-03-14 18:24:27 -0300

    + First commit of pyrentals files.

---
+++ pyrentals.py
@@ -0,0 +1,33 @@
+
+class Cart:
+    Rentals = []
+    PriceList = {"Hour": 5, "Day": 20, "Month": 60}
+
+    def empty(self):
+        return 0 == len(self.Rentals)
+
+    def calculate_price(self, price_list = [], unit_prices = [], enable_discount = True):
+        total_price = 0
+        if 0 == len(price_list):
+            price_list = self.PriceList
+
+        for iRental in range(len(self.Rentals)):
+            rental = self.Rentals[iRental]
+            type_text = rental["Type"]
+            rental_price = price_list[rental["Type"]] * rental["Time"]
+            unit_prices.append(rental_price)
+            print("\n- Rental #%s: Type: %s, %ss: %s, Price: %s.", 1 + iRental, type_text, type_text, rental["Time"]
+                  , rental_price)
+            total_price += rental_price
+
+        rental_count = len(unit_prices)
+        discount_qualified = rental_count in range(3, 5)
+        discount_applied = enable_discount and discount_qualified
+        if discount_applied:
+            discount_value = total_price * .3
+            price_with_discount = total_price - discount_value
+            print("\n-- Family Package Discount --""\nRental Price: " + total_price + "\nDiscount: " + discount_value
+                  + " \nFinal price: " + price_with_discount + "\n")
+            total_price = price_with_discount
+        return total_price
+
---
+++ test_pyrentals.py
@@ -0,0 +1,35 @@
+from pyrentals import Cart
+import unittest
+
+class Test_test_pyrentals(unittest.TestCase):
+    test_cart_instance = Cart()
+
+    def test_method_empty(self):
+        self.test_cart_instance = Cart()
+        self.test_cart_instance.Rentals = {}
+        return self.assertTrue(self.test_cart_instance.empty(), "Cart should be empty when it's just created.")
+
+    def test_method_not_empty(self):
+        self.test_cart_instance = Cart()
+        test_rental = {"Type": "Hour", "Time": 2}
+        self.test_cart_instance.Rentals = [test_rental, ]
+        return self.assertFalse(self.test_cart_instance.empty(), "Cart shouldn't be empty if we just added test_rental.")
+
+    def test_calculate_price_empty(self):
+        self.test_cart_instance = Cart()
+        return self.assertEqual(0, self.test_cart_instance.calculate_price())
+
+    def test_price_list(self):
+        self.test_cart_instance = Cart()
+        price_list = {"Hour": 5, "Day": 15, "Month": 60}
+        test_rental = {"Type": "Day", "Time": 2}
+        self.test_cart_instance.Rentals = [test_rental, test_rental, ]
+        output_prices = []
+        final_price = self.test_cart_instance.calculate_price(price_list, output_prices)
+        for price in output_prices:
+            self.assertEqual(30, price)
+        self.assertEqual(60, final_price)
+        return
+
+if __name__ == '__main__':
+    unittest.main()
