4d2aa1cc736a690dc0192d0cda095714031a2f05
asm128 <pabloarielz@outlook.com>
2019-03-14 20:56:56 -0300

    + Improved string formatting.

--- pyrentals.py
+++ pyrentals.py
@@ -16,8 +16,8 @@ class Cart:
             type_text = rental["Type"]
             rental_price = price_list[rental["Type"]] * rental["Time"]
             unit_prices.append(rental_price)
-            print("\n- Rental #%s: Type: %s, %ss: %s, Price: %s.", 1 + iRental, type_text, type_text, rental["Time"]
-                  , rental_price)
+            print("\n- Rental #{:02d}: Type: {}, {}s: {}, Price: {:2.2f}.".format(1 + iRental, type_text, type_text, rental["Time"]
+                  , rental_price))
             total_price += rental_price
 
         rental_count = len(unit_prices)
@@ -26,8 +26,14 @@ class Cart:
         if discount_applied:
             discount_value = total_price * .3
             price_with_discount = total_price - discount_value
-            print("\n-- Family Package Discount --""\nRental Price: " + total_price + "\nDiscount: " + discount_value
-                  + " \nFinal price: " + price_with_discount + "\n")
+            print("\n-- Family Package Discount --""\nRental Price: {:2.2f}\nDiscount: {:2.2f}\nFinal price: {:2.2f}\n".format(total_price, discount_value, 
+                  price_with_discount))
             total_price = price_with_discount
         return total_price
+
+#if __name__ == '__main__':
+#    cart = Cart()
+#    test_rental = {"Type": "Day", "Time": 10}
+#    cart.Rentals = [test_rental, test_rental, test_rental, test_rental, test_rental, ]
+#    cart.calculate_price()
 
