a0a5268f94d3c338147afe61ed628aa54737d60a
asm128 <pabloarielz@outlook.com>
2019-03-15 05:15:39 -0300

    + Added test_output_prices().

--- pyrentals/test_pyrentals.py
+++ pyrentals/test_pyrentals.py
@@ -40,9 +40,19 @@ class Test_test_pyrentals(unittest.TestCase):
         final_price = self.test_cart_instance.calculate_price(price_list, output_prices)
         raw_price = sum([x["Time"] * price_list[x["Type"]] for x in self.test_cart_instance.Rentals])
         self.assertLess(final_price, raw_price)
-        self.assertEqual(raw_price, sum(output_prices))
-        self.assertTrue(final_price == raw_price - raw_price * .3)
-        return
+        return self.assertTrue(final_price == raw_price - raw_price * .3)
+        
+
+    def test_output_prices(self):
+        self.test_cart_instance = Cart()
+        price_list = {"Hour": 5, "Day": 15, "Month": 60}
+        test_rental = {"Type": "Day", "Time": 2}
+        self.test_cart_instance.Rentals = [test_rental, test_rental, test_rental, ]
+        output_prices = []
+        final_price = self.test_cart_instance.calculate_price(price_list, output_prices)
+        raw_price = sum([x["Time"] * price_list[x["Type"]] for x in self.test_cart_instance.Rentals])
+        return self.assertEqual(raw_price, sum(output_prices))
+
 
 if __name__ == '__main__':
     unittest.main()
